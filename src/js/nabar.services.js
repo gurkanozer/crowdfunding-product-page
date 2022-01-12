6d9621e1a48b8ef91443176693a4b36d01fa12a5
gurkanozer <grknzr2609@gmail.com>
2022-01-12 15:08:09 +0300

    first commit

---
+++ src/js/nabar.services.js
@@ -0,0 +1,11 @@
+export class NavbarService {
+    constructor(navbar){
+        this.navbar = navbar;
+    }
+    close(){
+        //close navbar...
+    }
+    open(){
+        //open Navbar...
+    }
+}
